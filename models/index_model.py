a968fcac17811f8525c240a9317ce8dac61d9806
Noemi <agaudi@live.com.mx>
2025-01-07 19:36:12 -0600

    Busqueda +por titulo

--- models/index_model.py
+++ models/index_model.py
@@ -1,3 +1,16 @@
 class IndexModel:
+<<<<<<< HEAD
+    from models import conexion
+
+    con = conexion.Conexion()
+    
     def search(self, data):
         print(f"Buscando información para: {data}")
+        resultados = self.con.buscar_xtitulo(data)
+        for row in resultados:
+                    print(row)
+        
+=======
+    def search(self, data):
+        print(f"Buscando información para: {data}")
+>>>>>>> parent of 0c8761e (Actualización 07/01/25)
